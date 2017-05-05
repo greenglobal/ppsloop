f1ead6eb9fbfd8da4d2302fb84be47d279aa2749
Dong Nguyen <ndaidong@gmail.com>
2017-05-05 13:29:09 +0700

    v0.1.51
    - Update release strategy

--- builder/RM.js
+++ builder/RM.js
@@ -20,6 +20,8 @@ const ENV = process.env.NODE_ENV || 'development'; // eslint-disable-line
 const SOURCE = path.join(__dirname, '../src');
 const DIST = path.join(__dirname, ENV === 'production' ? '../docs' : '../dist');
 
+const {version} = require(path.join(__dirname, '../package.json'));
+
 const JSFILES = [
   'vendor/stabilize.min.js',
   'vendor/doc.min.js',
@@ -46,7 +48,10 @@ var processCSS = async () => {
   try {
     info('Start compiling CSS...');
     let s = await compileCSS(CSSFILES, `${SOURCE}/widget`);
-    writeFile(`${DIST}/widget/ppsloop.widget.css`, s);
+    writeFile(`${DIST}/widget/ppsloop.widget.css`, [
+      `/** v.${version} */`,
+      s
+    ].join('\n'));
     info('Finish compiling CSS...');
   } catch (err) {
     error(err);
@@ -60,6 +65,7 @@ var processJS = async () => {
     let s = await compileJS(JSFILES, `${SOURCE}/widget`);
     let data = readFile(`./dist/widget/ppsloop.widget.json`);
     writeFile(`${DIST}/widget/ppsloop.widget.js`, [
+      `/** v.${version} */`,
       s,
       `PPSW.init(${data});`
     ].join(';'));

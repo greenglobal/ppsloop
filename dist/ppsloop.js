d710303eb52075088fe672ae4d2245ca10d5b5f1
Dong Nguyen <ndaidong@gmail.com>
2017-05-17 09:24:38 +0700

    v0.4.1
    - Hide project view all button when it's disable

--- dist/ppsloop.js
+++ dist/ppsloop.js
@@ -1,4 +1,4 @@
-/** v0.4.0 */
+/** v0.4.1 */
 Array.prototype.includes||Object.defineProperty(Array.prototype,"includes",{value:function(f,g){if(null==this)throw new TypeError('"this" is null or not defined');var e=Object(this),b=e.length>>>0;if(0===b)return!1;for(var a=g|0,a=Math.max(0<=a?a:b-Math.abs(a),0);a<b;){var c=e[a],d=f;if(c===d||"number"===typeof c&&"number"===typeof d&&isNaN(c)&&isNaN(d))return!0;a++}return!1}});
 
 // stabilize.js@1.2.16, by @ndaidong - built on Fri, 24 Feb 2017 11:33:05 GMT - published under MIT license
